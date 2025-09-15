5bad3490a8c097ca12ed76087862188380b2ef1a
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-09-15 12:55:04 +0530

    Navbar column gap

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,8 +7,8 @@ function Navbar() {
         <div>
             <nav className='navbar navbar-expand-lg border bg-primary' id='navbar'>
                 <NavLink to='/index' className='navbar navbar-brand text-white' id='Logo'> Kalaivanan</NavLink>
-                <div className=' navbar navbar ml-auto'>
-                    <ul className='navbar-nav'>
+                <div className=' navbar navbar ml-auto' style={{display:'flex',flex:'0.4'}}>
+                    <ul className='navbar-nav' style={{columnGap:'30px'}}>
                         <li><NavLink to='index ' className='nav-link'> Home</NavLink></li>
                         <li><NavLink to='about ' className='nav-link'> About</NavLink></li>
                         <li><NavLink to='/skills' className='nav-link'> Skills</NavLink></li>
