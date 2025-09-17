e8b46bbfea56e2eab684ec5be64618fc18e873b2
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-09-17 16:22:49 +0530

    Skills

---
+++ src/Components/Skills copy.jsx
@@ -0,0 +1,57 @@
+// import React from "react";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// function Skills() {
+//   return (
+//     <section id="skills" className="container mt-5">
+//       <h2 className="text-center mb-4">My Skills</h2>
+      
+//       <div className="mb-3">
+//         <h5>HTML / CSS</h5>
+//         <div className="progress">
+//           <div className="progress-bar bg-success" role="progressbar" style={{width: "90%"}}>
+//             90%
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className="mb-3">
+//         <h5>JavaScript</h5>
+//         <div className="progress">
+//           <div className="progress-bar bg-info" role="progressbar" style={{width: "80%"}}>
+//             80%
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className="mb-3">
+//         <h5>React.js</h5>
+//         <div className="progress">
+//           <div className="progress-bar bg-warning" role="progressbar" style={{width: "75%"}}>
+//             75%
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className="mb-3">
+//         <h5>Java</h5>
+//         <div className="progress">
+//           <div className="progress-bar bg-danger" role="progressbar" style={{width: "70%"}}>
+//             70%
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className="mb-3">
+//         <h5>SQL / MySQL</h5>
+//         <div className="progress">
+//           <div className="progress-bar bg-primary" role="progressbar" style={{width: "65%"}}>
+//             65%
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default Skills;
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,10 +1,23 @@
-
 import React from 'react'
 
- const Skills = () => {
+const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className='container mt-5'>
+        <h2 className='text-center' style={{fontWeight:'bold'}}>My Skills</h2>
+      <div>
+        <h4>HTML / CSS</h4>
+        <div className='progress'>
+            <div className="progress-bar bg-success" role='progressbar' style={{width:'85%'}}> 85%</div>
+        </div>
+      </div>
+      <div>
+        <h4>JavaScript</h4>
+        <div className="progress">
+            <div className="progress-bar bg-info" role='progressbar' style={{width:'60%'}}></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Skills;
+export default Skills
